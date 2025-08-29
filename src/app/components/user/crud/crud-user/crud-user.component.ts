6dead05edd1b69efe1fc69ad6a40be7802ce28fe
ameapps <git.stefano@outlook.it>
2025-08-29 10:37:16 +0200

    implmementato componente add/edit user

--- src/app/components/user/crud/crud-user/crud-user.component.ts
+++ src/app/components/user/crud/crud-user/crud-user.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../../../../shared/services/common/common.service';
+import { User } from '../../../../shared/models/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crud-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './crud-user.component.html',
   styleUrl: './crud-user.component.scss'
 })
-export class CrudUserComponent {
+export class CrudUserComponent implements OnInit {
+  user: User = new User();
+  pageTitle = 'Aggiungi nuovo utente';
+  success = false;
 
+  constructor(
+    public common: CommonService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    if (this.router.url.includes('/user/edit')) {
+      this.pageTitle = 'Modifica utente';
+      if (this.common.lastLoggedUser) {
+        // Copia i dati dell'utente loggato
+        this.user = { ...this.common.lastLoggedUser };
+      }
+    }
+  }
+
+  onSubmit() {
+    // Qui puoi aggiungere la logica per salvare o aggiornare l'utente
+    if (this.pageTitle === 'Aggiungi nuovo utente') {
+      // Logica per aggiunta utente
+      console.log('Nuovo utente:', this.user);
+    } else {
+      // Logica per modifica utente
+      this.common.lastLoggedUser = { ...this.user };
+      this.common.saveUserSession();
+      console.log('Utente modificato:', this.user);
+    }
+    this.success = true;
+    setTimeout(() => (this.success = false), 2000);
+  }
 }
